8a6ca713d1c200830d2d46ae3dded6fc05893021
CakeAL <1742507190@qq.com>
2024-06-12 13:26:31 +0800

    🐞 fix(app): 修了多个计时器问题，加了深色模式，换了等宽字体

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     NButton: typeof import('naive-ui')['NButton']
+    NConfigProvider: typeof import('naive-ui')['NConfigProvider']
+    NGlobalStyle: typeof import('naive-ui')['NGlobalStyle']
     NProgress: typeof import('naive-ui')['NProgress']
     NSelect: typeof import('naive-ui')['NSelect']
     NSpace: typeof import('naive-ui')['NSpace']
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from "vue";
 import { repeat } from "seemly";
+import { darkTheme } from "naive-ui";
 
 const remainSeconds = ref<number>(300);
 const setMinutes = ref<number>(5);
 const setWarnMinute = ref<number>(1);
-// const currentState = ref<boolean>(false); // false 暂停 ture 开始
+const currentState = ref<boolean>(false); // false 暂停 ture 开始
 const progressState = ref<string>("success");
 const minutesOptions = repeat(61, undefined).map((_, i) => ({
   label: String(i),
   value: i,
 }));
-
 const interval = ref<number>(0); // 用来存储计时器
+const theme = ref<any | undefined>(darkTheme);
 
 onMounted(() => {
   return () => clearInterval(interval.value);
@@ -20,6 +21,7 @@ onMounted(() => {
 
 const start = () => {
   window.speechSynthesis.speak(new SpeechSynthesisUtterance("请开始答辩"));
+  currentState.value = true;
   interval.value = setInterval(() => {
     if (remainSeconds.value <= 1) {
       clearInterval(interval.value);
@@ -40,12 +42,22 @@ const start = () => {
 
 const pause = () => {
   clearInterval(interval.value);
+  currentState.value = false;
 };
 
 const reset = () => {
   clearInterval(interval.value);
   remainSeconds.value = setMinutes.value * 60;
   progressState.value = "success";
+  currentState.value = false;
+};
+
+const changeTheme = () => {
+  if (theme.value === undefined) {
+    theme.value = darkTheme;
+  } else {
+    theme.value = undefined;
+  }
 };
 
 const handleUpdateSetMinutes = () => {
@@ -58,74 +70,101 @@ const minutes = computed(() => {
     return "0" + mins;
   } else return mins;
 });
+
 const seconds = computed(() => {
   let secs = remainSeconds.value % 60;
   if (secs < 10) {
     return "0" + secs;
   } else return secs;
 });
+
 const percentage = computed(() => {
   return 100 - (remainSeconds.value / setMinutes.value / 60) * 100;
 });
 </script>
 
 <template>
-  <div class="container">
-    <div class="countdown">
-      <span>{{ minutes }} : {{ seconds }}</span>
-      <n-progress
-        type="line"
-        :status="progressState"
-        :percentage="percentage"
-        :indicator-placement="'inside'"
-      />
+  <n-config-provider :theme="theme">
+    <n-global-style />
+    <div class="container">
+      <div class="countdown">
+        <span>{{ minutes }}:{{ seconds }}</span>
+        <n-progress
+          type="line"
+          height="50"
+          :status="progressState"
+          :percentage="percentage"
+          :indicator-placement="'inside'"
+        />
+      </div>
+      <div>
+        <n-space justify="center">
+          <br />
+          <n-button
+            strong
+            secondary
+            round
+            type="primary"
+            @click="start"
+            size="large"
+            :disabled="currentState"
+          >
+            开始
+          </n-button>
+          <n-button
+            strong
+            secondary
+            round
+            type="info"
+            @click="pause"
+            size="large"
+            :disabled="!currentState"
+          >
+            暂停
+          </n-button>
+          <n-button
+            strong
+            secondary
+            round
+            type="warning"
+            @click="reset"
+            size="large"
+          >
+            重置
+          </n-button>
+          <n-button strong secondary round @click="changeTheme" size="large">
+            切换亮/暗
+          </n-button>
+          <br />
+        </n-space>
+        <span>设置倒计时时长（分钟）</span>
+        <n-select
+          v-model:value="setMinutes"
+          :options="minutesOptions"
+          @update:value="handleUpdateSetMinutes"
+        />
+        <span>设置剩余时间提醒（分钟）</span>
+        <n-select v-model:value="setWarnMinute" :options="minutesOptions" />
+      </div>
     </div>
-    <n-space justify="center">
-      <n-button
-        strong
-        secondary
-        round
-        type="primary"
-        @click="start"
-        size="large"
-      >
-        开始
-      </n-button>
-      <n-button strong secondary round type="info" @click="pause" size="large">
-        暂停
-      </n-button>
-      <n-button
-        strong
-        secondary
-        round
-        type="warning"
-        @click="reset"
-        size="large"
-      >
-        重置
-      </n-button>
-      <br />
-    </n-space>
-    <span>设置倒计时时长（分钟）</span>
-    <n-select
-      v-model:value="setMinutes"
-      :options="minutesOptions"
-      @update:value="handleUpdateSetMinutes"
-    />
-    <span>设置剩余时间提醒（分钟）</span>
-    <n-select v-model:value="setWarnMinute" :options="minutesOptions" />
-  </div>
+  </n-config-provider>
 </template>
 
 <style scoped>
+.container {
+  display: block;
+}
 
 .countdown {
   width: 100vw;
   height: 80vh;
+  text-align: center;
 }
 
 .countdown span {
-  font-weight: bold;
+  font-family: monospace;
+  font-weight: 900;
+  line-height: 70vh;
   font-size: 30vw;
 }
 </style>
